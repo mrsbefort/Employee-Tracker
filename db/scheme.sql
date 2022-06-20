1a63dc9f9030d7721fbda96324503782d39a92db
Gabrielle Befort <mrsbefort@gmail.com>
2022-06-19 20:41:16 -0400

    schema.sql

--- db/scheme.sql
+++ db/scheme.sql
@@ -1,31 +1,24 @@
--- Drops the task_saver_db if it already exists --
 DROP DATABASE IF EXISTS employeeTracker_db;
-
--- Create the database task_saver_db and specified it for use.
 CREATE DATABASE employeeTracker_db;
 
 USE employeeTracker_db;
 
--- Create the table tasks.
 CREATE TABLE department (
-  id int NOT NULL AUTO_INCREMENT,
-  name varchar(30) NOT NULL,
-  PRIMARY KEY (id)
+  id INTEGER NOT NULL,
+  name varchar(30),
 );
 
 CREATE TABLE role (
-    id INTEGER NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
-    PRIMARY KEY (id)
+    id INTEGER NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INTEGER NOT NULL, 
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT,
-    PRIMARY KEY (id)
+    id INTEGER NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER NOT NULL,
 )
